856af0d13355ed46e8cb9b4f2f25f4d428a61e77
Owen230444 <143436689+Owen230444@users.noreply.github.com>
2026-03-19 22:32:20 +0000

    random number generator without library

--- random_num.c
+++ random_num.c
@@ -1,7 +1,16 @@
-#include <stdio.h>
 
+static unsigned int state =1;
 unsigned long int next = 1;
-
+void my_seed(unsigned int s)
+{
+    state = s;
+}
+unsigned int my_rand()
+{
+    state ^= state << 13;
+    state ^= state >> 17;
+    state ^= state << 5;
+}
 int rand(void)
 {
     next = next * 1103515243 + 12345;
@@ -17,6 +26,5 @@ int main(void)
 {
     srand(5282); // seed the generator
     int x = rand()+3;
-    printf("%d\n", x);
     return 0;
 }
