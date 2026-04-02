649839e46ad3ed1310783cb8e89932334fc04b84
an3shen <164962199+an3shen@users.noreply.github.com>
2026-04-02 22:44:28 +0000

    Audio input is finished , just need to test

--- Audio_Input/audio_input.c
+++ Audio_Input/audio_input.c
@@ -1,18 +1,17 @@
-
 unsigned int sound_detected = 0;
 unsigned int sound_time = 0;
 #define SOUND_THRESHOLD 2800
 
 void main(void)
 {
-    // 1. Power microphone + op-amp
+    // Power microphone + op-amp
     P6DIR |= BIT4;      // P6.4 output
     P6OUT |= BIT4;      // turn ON mic + op-amp
 
-    // 2. Configure ADC12 for A5 (audio input)
-    ADC12CTL0 = ADC12SHT0_2 | ADC12ON;
-    ADC12CTL1 = ADC12SHP | ADC12CONSEQ_2;
-    ADC12MCTL0 = ADC12INCH_5;
+    //Configure ADC12 for A5 (audio input)
+    ADC12CTL0 = ADC12SHT0_2 | ADC12ON; // Need to change sample time: scurrently sampling 16 ADC12CLK cycles
+    ADC12CTL1 = ADC12SHP | ADC12SSEL_3 |ADC12CONSEQ_2; //SAMPSON sourced from sample timer, sequence of reapeat signal channel, set to SMCLK
+    ADC12MCTL0 = ADC12INCH_5; //Selects A5
     ADC12CTL0 |= ADC12ENC | ADC12SC;              // enable ADC
 
 }
