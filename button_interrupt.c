590d466647fc7f0c5f590e993865d4cb024dbea5
an3shen <164962199+an3shen@users.noreply.github.com>
2026-04-02 21:51:08 +0000

    Changed cod so random_num works with code

--- button_interrupt.c
+++ button_interrupt.c
@@ -5,8 +5,7 @@ void button_interrupt(void)__interrupt[PORT2_VECTOR]
         if (state == 0)
         {
             TA0CTL |= TACLR;
-            unsigned int delay_ms = x % 3000 + 500; //Replace with random_num
-            TA0CCR1 = TA0R + delay_ms * 1000;
+            TA0CCR1 = x * 1000;
             state = 1;
         }
         else if (state == 2)
