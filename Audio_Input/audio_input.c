85f34bb6339851afa27697171408011296f6831a
an3shen <164962199+an3shen@users.noreply.github.com>
2026-04-02 22:29:28 +0000

    Audio input is finished , just need to test

--- Audio_Input/audio_input.c
+++ Audio_Input/audio_input.c
@@ -1,3 +1,8 @@
+
+unsigned int sound_detected = 0;
+unsigned int sound_time = 0;
+#define SOUND_THRESHOLD 2800
+
 void main(void)
 {
     // 1. Power microphone + op-amp
@@ -5,17 +10,9 @@ void main(void)
     P6OUT |= BIT4;      // turn ON mic + op-amp
 
     // 2. Configure ADC12 for A5 (audio input)
-    ADC12CTL0 = ADC12SHT0_2 | ADC12ON;   // sample time + ADC on
-    ADC12CTL1 = ADC12SHP;                // use sampling timer
-    ADC12MCTL0 = ADC12INCH_5;            // channel A5
-    ADC12CTL0 |= ADC12ENC;               // enable ADC
+    ADC12CTL0 = ADC12SHT0_2 | ADC12ON;
+    ADC12CTL1 = ADC12SHP | ADC12CONSEQ_2;
+    ADC12MCTL0 = ADC12INCH_5;
+    ADC12CTL0 |= ADC12ENC | ADC12SC;              // enable ADC
 
-    while (1)
-    {
-        ADC12CTL0 |= ADC12SC;            // start conversion
-        while (ADC12CTL1 & ADC12BUSY);   // wait
-        unsigned int sample = ADC12MEM0; // audio sample
-
-        __no_operation();                // breakpoint here to inspect sample
-    }
 }
--- Audio_Input/input_interrupt.c
+++ Audio_Input/input_interrupt.c
@@ -1,1 +1,10 @@
 void ADC_Interrupt(void)__interrupt[ADC12_VECTOR]
+{
+    unsigned int sample = ADC12MEM0;
+
+    if (!sound_detected && sample > SOUND_THRESHOLD)
+    {
+        sound_time = TA0R;
+        sound_detected = 1;
+    }
+}
